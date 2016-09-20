44bcdfb4ed03ac35300a3a02681572723cae30d0
Vikash Kodati <vikash.kodati@gmail.com>
2016-09-20 15:57:36 -0700

    updated bookstore service url

--- reading/src/main/java/hello/BookService.java
+++ reading/src/main/java/hello/BookService.java
@@ -11,7 +11,7 @@ public class BookService {
   @HystrixCommand(fallbackMethod = "reliable")
   public String readingList() {
     RestTemplate restTemplate = new RestTemplate();
-    URI uri = URI.create("http://localhost:8082/bookstore/recommended");
+    URI uri = URI.create("http://kodati-bookstore.apps.px-01.cf.t-mobile.com/bookstore/recommended");
 
     return restTemplate.getForObject(uri, String.class);
   }
